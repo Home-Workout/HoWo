e803bf9d9c9b3dd923a2909111d4ecec64fb4b0f
Damaris Claros <damarisclarosalvarez@gmsil.com>
2021-04-26 18:22:41 -0400

    conexion con la BD ejercicio

--- script/validarDatosEjercicio.js
+++ script/validarDatosEjercicio.js
@@ -1,3 +1,4 @@
+const dbE = firebase.firestore(); 
 function validateForm() {
     var campoNombre = document.forms["formularioEjercicio"]["eName"].value;
     var campoDescripcion = document.forms["formularioEjercicio"]["eName"].value;
@@ -18,4 +19,70 @@ function validateForm() {
         document.forms.reset;
     }
 
+}
+document.getElementById("enviarEjercicio").addEventListener("click", async(e)=> {
+    const registroEjercicio = document.getElementById("formulario1");
+    const nombreE = registroEjercicio["name1"].value;
+    const descripcion = registroEjercicio["descripcion"].value;
+    const imageRef = subirImagen();
+    const videoRef = subirVideo();
+    const linkV = registroEjercicio["linkVideo"].value;
+    const nivelE = registroEjercicio["nivel"].value;
+    const areaT = registroEjercicio["areaT"].value;
+
+
+    e.preventDefault();
+    
+    const respuestaE = await dbE.collection("Agregar_Ejercicio").doc().set({
+        nombreE, 
+        descripcion,
+        imageRef,
+        videoRef,
+        linkV,
+        nivelE,
+        areaT
+
+
+
+    })
+
+    console.log(respuestaE)
+
+});
+
+function subirImagen() {
+
+    var image = document.getElementById("image").files[0];
+
+    var imageName = image.name;
+
+
+    var storageRef = firebase.storage().ref('Imagenes/' + image.name);
+    var referencia = 'Imagenes/ ' + image.name;
+    var uploadTask = storageRef.put(image);
+
+    uploadTask.on('state_changed', function (snapshot) {
+
+    }
+    )
+    return referencia
+    
+}
+
+function subirVideo() {
+
+    var video = document.getElementById("video").files[0];
+
+    var videoName = video.name;
+
+
+    var storageRef = firebase.storage().ref('Videos/' + video.name);
+    var referenciaVideo = 'Videos/ ' + video.name;
+    var uploadTask = storageRef.put(video);
+
+    uploadTask.on('state_changed', function (snapshot) {
+
+    }
+    )
+    return referenciaVideo
 }
