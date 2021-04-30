81fab95f91307ba79de48c5a9b3081bb3be5f6f1
Rafael Nogales <nogalesrafael1@gmail.com>
2021-04-30 09:55:31 -0400

    añadido el portafolio para las rutinas

--- script/app.js
+++ script/app.js
@@ -40,3 +40,60 @@ function abrir2(){
 function cerrar2(){
     document.getElementById("vent2").style.display="none";
 }
+
+/*portafolio*/
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+/*var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    console.log(current.namedItem); 
+    console.log(current[0].className);
+    current[0].className = current[0].className.replace(" active","");
+    console.log(current[0].className);
+    this.className += " active";
+  });
+}*/
+
+/*$(function() {
+  var botones = $(".container button");
+  botones.click(function() {
+    botones.removeClass('active');
+    $(this).addClass('active');
+  });
+});*/
