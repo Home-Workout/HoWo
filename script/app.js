56667db4da6eeead0be94f839c849b31d4dbd419
ipoandres08 <andresiporre07@gmail.com>
2021-05-01 16:09:39 -0600

    cambio en pag principal

--- script/app.js
+++ script/app.js
@@ -1,3 +1,32 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 5,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
 function abrir(){
     document.getElementById("vent").style.display="block";
     cerrar2();
@@ -12,3 +41,60 @@ function abrir2(){
 function cerrar2(){
     document.getElementById("vent2").style.display="none";
 }
+
+/*portafolio*/
+
+filterSelection("nature")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+/*var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    console.log(current.namedItem); 
+    console.log(current[0].className);
+    current[0].className = current[0].className.replace(" active","");
+    console.log(current[0].className);
+    this.className += " active";
+  });
+}*/
+
+/*$(function() {
+  var botones = $(".container button");
+  botones.click(function() {
+    botones.removeClass('active');
+    $(this).addClass('active');
+  });
+});*/
