124411afcaeb620f5771ead08fffbd63a3fde493
ipoandres08 <andresiporre07@gmail.com>
2021-05-02 22:27:07 -0600

    creacion de .php

---
+++ agregarEjercicios.php
@@ -0,0 +1,1 @@
+<?php require_once("public/AgregarEjercicio.html") ?>
--- index.php
+++ index.php
@@ -1,3 +1,1 @@
 <?php require_once("public/index.html") ?>
-<?php require_once("public/Registrarse.html") ?>
-<?php require_once("public/AgregarEjercicio.html") ?>
