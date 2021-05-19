c02564f8ec98ef0959777e2e862e39cabcc1e781
Damaris Claros <damarisclarosalvarez@gmsil.com>
2021-05-18 20:57:25 -0400

    correccion consulta

--- script/verRutina.js
+++ script/verRutina.js
@@ -62,13 +62,7 @@ async function consulta() {
         // `url` is the download URL for 'images/stars.jpg'
 
         // This can be downloaded directly:
-        var xhr = new XMLHttpRequest();
-        xhr.responseType = 'blob';
-        xhr.onload = function(event) {
-            var blob = xhr.response;
-        };
-        xhr.open('GET', url);
-        xhr.send();
+        
 
         // Or inserted into an <img> element:
         var img = document.getElementById('imgID');
@@ -93,13 +87,7 @@ function avanzar() {
             // `url` is the download URL for 'images/stars.jpg'
 
             // This can be downloaded directly:
-            var xhr = new XMLHttpRequest();
-            xhr.responseType = 'blob';
-            xhr.onload = function(event) {
-                var blob = xhr.response;
-            };
-            xhr.open('GET', url);
-            xhr.send();
+            
 
             // Or inserted into an <img> element:
             var img = document.getElementById('imgID');
@@ -127,13 +115,7 @@ function retroceder() {
             // `url` is the download URL for 'images/stars.jpg'
 
             // This can be downloaded directly:
-            var xhr = new XMLHttpRequest();
-            xhr.responseType = 'blob';
-            xhr.onload = function(event) {
-                var blob = xhr.response;
-            };
-            xhr.open('GET', url);
-            xhr.send();
+            
 
             // Or inserted into an <img> element:
             var img = document.getElementById('imgID');
